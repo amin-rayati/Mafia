db3afdbc24473753a3c1fce4794f1d3a010358f3
Amin Rayati <87262713+amin-rayati@users.noreply.github.com>
2025-02-04 02:46:51 +0330

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true,  // این گزینه باید فعال بشه برای استفاده از پوشه app
+  },
 };
+
+export default nextConfig;
