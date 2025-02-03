cd2ac69afdd7aa6e0ef4318b327a6ab044b5110c
Amin Rayati <87262713+amin-rayati@users.noreply.github.com>
2025-02-04 02:54:02 +0330

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,4 @@
-import MainSlider from "@/components/Home/MainSlider";
-import About from "@/components/Home/About";
-import Feature from "@/components/Home/Feature";
-import BlogHome from "@/components/Home/BlogHome";
+
 export default function Home() {
   return (
     <div>amin</div>
